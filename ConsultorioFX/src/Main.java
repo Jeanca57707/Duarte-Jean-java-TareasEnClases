e3cc05bdfb3358220d07fdf1344d1116a694e492
Jeanca57707 <jean_duarte@ucne.edu.do>
2026-07-19 16:14:50 -0400

    Creando la Estructura

---
+++ ConsultorioFX/src/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application{
+
+
+    @Override
+    public void start(Stage primarStage)throws Exception{
+
+
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        launch(args);
+    }
+
+}
