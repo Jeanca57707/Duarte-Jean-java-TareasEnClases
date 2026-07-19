bb15a0c1d1726c6f8463781c0a2de0ba1e2c44b4
Jeanca57707 <jean_duarte@ucne.edu.do>
2026-07-19 16:33:30 -0400

    Clase Navegación creada

--- ConsultorioFX/src/model/Consulta.java
+++ ConsultorioFX/src/model/Consulta.java
@@ -1,5 +1,59 @@
 package model;
 
 public class Consulta {
+
+    private int codigo;
+    private String nombre;
+    private String telefono;
+    private String medico;
+    private String especialidad;
+    private String motivoConsulta;
+    private String estado;
+    
+    public Consulta(int codigo, String nombre, String telefono, String medico, String especialidad,
+            String motivoConsulta, String estado) {
+
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.medico = medico;
+        this.especialidad = especialidad;
+        this.motivoConsulta = motivoConsulta;
+        this.estado = estado;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getMedico() {
+        return medico;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public String getMotivoConsulta() {
+        return motivoConsulta;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    @Override
+    public String toString(){
+
+        return nombre;
+    }
     
 }
